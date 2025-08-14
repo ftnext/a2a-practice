1fe0f303020e8ef20a6398f9da75bf8ef14f2af5
ftnext <takuyafjp+develop@gmail.com>
2025-08-14 22:18:42 +0900

    [feat] Fix for A2A 0.3.0

--- a2a-samples/helloworld/call_from_adk/agent.py
+++ a2a-samples/helloworld/call_from_adk/agent.py
@@ -1,5 +1,5 @@
 from google.adk.agents.remote_a2a_agent import RemoteA2aAgent
 
 root_agent = RemoteA2aAgent(
-    name="Parrot_Agent", agent_card="http://0.0.0.0:9999/.well-known/agent.json"
+    name="Parrot_Agent", agent_card="http://0.0.0.0:9999/.well-known/agent-card.json"
 )
